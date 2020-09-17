4e5f3236da4080cae61fa068943a0871c87e3835
领 <qianlingvp@163.com>
2020-09-17 17:12:10 +0800

    [fix]优化样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,26 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <h6>{{list.names}}</h6><input type="text" v-model="list.names" @keyup.enter="addList">
+  <div class="my-header-content">
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <h6>{{list.names}} <input type="text" v-model="list.names" @keyup.enter="addList" /></h6>
+  </div>
   <ul>
-    <li v-for="item in list.dataList" :key="item.id"><i :class="item.active?'my-actived':'my-disable'"/><span>{{item.name}}-{{item.id}}</span><button v-if="!item.active" @click="handleChangeActive(item.id)">激活</button><button v-else @click="handleChangeActive(item.id)">失活</button></li>
+    <li v-for="item in list.dataList" :key="item.id">
+      <i :class="item.active?'my-actived':'my-disable'" />
+      <span v-if="!item.active"><del>{{item.name}}-{{item.id}}</del></span>
+      <span v-else>{{item.name}}-{{item.id}}</span>
+      <button v-if="!item.active" @click="handleChangeActive(item.id)">激活</button>
+      <button v-else @click="handleChangeActive(item.id)">失活</button>
+    </li>
   </ul>
-  <h6>合计： {{total}}条</h6>
+  <div class="my-footer-list">
+    <h6>合计： {{total}}条</h6>
+  </div>
 </template>
 
 <script>
 import { ref } from "vue";
-import useAddList from './useAddList.js';
+import useAddList from "./useAddList.js";
 export default {
   name: "HelloWorld",
   props: {
@@ -18,11 +28,11 @@ export default {
   },
   setup() {
     const count = ref(1);
-    const {list, addList, total, handleChangeActive}=useAddList();
-    return {list, addList, total, count, handleChangeActive}
+    const { list, addList, total, handleChangeActive } = useAddList();
+    return { list, addList, total, count, handleChangeActive };
   },
 };
 </script>
 <style lang="less">
-  @import './index.less';
+@import "./index.less";
 </style>
