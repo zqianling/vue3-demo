5914a207a5c7bd9649dd5a6b06db7effa98c2754
领 <qianlingvp@163.com>
2020-09-17 17:54:04 +0800

    [feat]新增删除线

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,17 @@
   <div class="my-header-content">
     <h1>{{ msg }}</h1>
     <button @click="count++">count is: {{ count }}</button>
-    <h6>{{list.names}} <input type="text" v-model="list.names" @keyup.enter="addList" /></h6>
+    <h6>
+      {{list.names}}
+      <input type="text" v-model="list.names" @keyup.enter="addList" />
+    </h6>
   </div>
   <ul>
     <li v-for="item in list.dataList" :key="item.id">
       <i :class="item.active?'my-actived':'my-disable'" />
-      <span v-if="!item.active"><del>{{item.name}}-{{item.id}}</del></span>
+      <span v-if="!item.active">
+        <del>{{item.name}}-{{item.id}}</del>
+      </span>
       <span v-else>{{item.name}}-{{item.id}}</span>
       <button v-if="!item.active" @click="handleChangeActive(item.id)">激活</button>
       <button v-else @click="handleChangeActive(item.id)">失活</button>
